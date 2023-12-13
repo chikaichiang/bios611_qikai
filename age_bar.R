272e9d0b907df2baa851f7765c5bb98cb4528668
Qikai Jiang <chikai@ad.unc.edu>
2023-12-13 14:17:18 -0500

    Added bar plot for age

---
+++ age_bar.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(tidyr)
+d1<-read.csv("work/source_data/kag_risk_factors_cervical_cancer.csv")
+age<-table(d1$Age)
+dev.new()
+png(file="./figures/age_bar.png",
+    width=600, height=350)
+b <- barplot(age, beside=TRUE, ylim=c(0, max(age) + 15), 
+             main="Age Count", col=2:3, border=0)
+text(b, age + 5, age, font=2, col=2:3)
+dev.off()
+
